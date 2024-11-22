a88381a65b5189151a49bbda81cdcb9faadf11a1
Michael <mquinter88@gmail.com>
2024-11-22 17:52:14 -0500

    spring security chapter - implement basic registration and login

--- src/main/java/com/mcq/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/mcq/tacocloud/TacoCloudApplication.java
@@ -2,12 +2,17 @@ package com.mcq.tacocloud;
 
 import com.mcq.tacocloud.model.Ingredient;
 import com.mcq.tacocloud.repo.IngredientRepository;
+import com.mcq.tacocloud.security.AppUser;
+import com.mcq.tacocloud.security.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@Slf4j
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
@@ -15,18 +20,41 @@ public class TacoCloudApplication {
     }
 
     @Bean
-    public ApplicationRunner dataLoader(IngredientRepository repo) {
+    public ApplicationRunner dataLoader(
+            IngredientRepository ingredientRepo,
+            UserRepository userRepo,
+            PasswordEncoder passwordEncoder) {
+
         return args -> {
-            repo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-            repo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-            repo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-            repo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-            repo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-            repo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-            repo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-            repo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-            repo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+            // Load ingredients
+            ingredientRepo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+            ingredientRepo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+            ingredientRepo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+            ingredientRepo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+            ingredientRepo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+            ingredientRepo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+            ingredientRepo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+            ingredientRepo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+            ingredientRepo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+            ingredientRepo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+
+            // Load default user
+            if (userRepo.findByUsername("testuser") == null) {
+                AppUser user = new AppUser(
+                        "testuser",
+                        passwordEncoder.encode("password"),
+                        "Test User",
+                        "123 Main St",
+                        "Anytown",
+                        "State",
+                        "12345",
+                        "555-1234"
+                );
+                userRepo.save(user);
+                log.info("Default user 'testuser' created.");
+            } else {
+                log.info("Default user 'testuser' already exists.");
+            }
         };
     }
 
---
+++ src/main/java/com/mcq/tacocloud/security/AppUser.java
@@ -0,0 +1,69 @@
+package com.mcq.tacocloud.security;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = "password")
+@EqualsAndHashCode(exclude = "id")
+public class AppUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String fullname;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+
+    public AppUser(String username, String password, String fullname, String street, String city, String state, String zip, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
